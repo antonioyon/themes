7aba483d0e6b59076b2951b06589b33caca8f489
antonio yon <ant@antonioyon.com>
2020-08-31 17:32:06 +0000

    Plastic color scheme

---
+++ themes/Plastic.js
@@ -0,0 +1,42 @@
+terminal.ansiBlack				= '#21252B';
+terminal.ansiBlue					= '#61AFEF';
+terminal.ansiCyan					= '#56B6C2';
+terminal.ansiGreen				= '#98C379';
+terminal.ansiMagenta			= '#B57EDC';
+terminal.ansiRed					= '#E06C75';
+terminal.ansiYellow				= '#E9D16C';
+terminal.ansiWhite				= '#A9B2C3';
+terminal.ansiBrightBlack	= '#5F6672';
+terminal.ansiBrightBlue		= '#61AFEF';
+terminal.ansiBrightCyan		= '#56B6C2';
+terminal.ansiBrightGreen	= '#98C379';
+terminal.ansiBrightMagenta= '#B57EDC';
+terminal.ansiBrightRed		= '#E06C75';
+terminal.ansiBrightYellow = '#E9D16C';
+terminal.ansiBrightWhite	= '#A9B2C3';
+
+t.prefs_.set(
+	'color-palett-overrides', [
+		terminal.ansiBlack,
+		terminal.ansiRed,
+		terminal.ansiGreen,
+		terminal.ansiYellow,
+		terminal.ansiBlue,
+		terminal.ansiMagenta,
+		terminal.ansiCyan,
+		terminal.ansiWhite,
+		terminal.ansiBrightBlack,
+		terminal.ansiBrightRed,
+		terminal.ansiBrightGreen,
+		terminal.ansiBrightYellow,
+		terminal.ansiBrightBlue,
+		terminal.ansiBrightMagenta,
+		terminal.ansiBrightCyan,
+		terminal.ansiBrightWhite
+	]
+);
+
+t.prefs_.set('foreground-color', "#A9B2C3");
+t.prefs_.set('background-color', "#21252B");
+t.prefs_.set('cursor-color', "#A9B2C3");
+
